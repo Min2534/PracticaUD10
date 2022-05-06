66670425639650df766caaa76aabcda4f08c326c
Sergio Laín <lain20402@iesmarenostrum.com>
2022-05-06 19:54:25 +0200

    diseño y creacion de interfaz
    Hemos tenido que borrar todo lo anterior debido a que hemos tenido problemas con el git de Visual Studio

--- PracticaFinal/PracticaFinal/Form1.cs
+++ PracticaFinal/PracticaFinal/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PracticaUD10
+namespace PracticaFinal
 {
     public partial class Form1 : Form
     {
@@ -16,5 +16,6 @@ namespace PracticaUD10
         {
             InitializeComponent();
         }
+
     }
 }
